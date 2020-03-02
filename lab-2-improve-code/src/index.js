f7816c4724f62651be9b5966f04f4007968e71ee
Ildiko Toth <tothildikoeva@gmail.com>
2020-03-02 13:01:16 +0000

    adding prettier

--- lab-2-improve-code/src/index.js
+++ lab-2-improve-code/src/index.js
@@ -1,14 +1,21 @@
-
-module.exports = function greeting(i = 'my friend') {
-  if (typeof i == 'string') {
+module.exports = function greeting(i = "my friend") {
+  if (typeof i === "string") {
     i = [i];
   }
 
-out = 'Hello, ';
+  let nStr = "";
+  for (const j in i) {
+    nStr =
+      nStr +
+      i[j] +
+      (parseInt(j, 10) + 2 === i.length
+        ? ", and "
+        : parseInt(j, 10) + 1 === i.length
+        ? ""
+        : ", ");
+  }
 
-  nStr = '';
-  for (j in i ) { nStr = nStr + i[j] + ((parseInt(j)+2==i.length)?', and ': ((parseInt(j)+1==i.length)?'':', ')); }
+  if (nStr.toUpperCase() === nStr) return `HELLO ${nStr}!`;
 
-    if (nStr.toUpperCase() === nStr) return "HELLO " + nStr +  '!'
-
-  return "Hello, " + nStr +  '.'}
+  return `Hello, ${nStr}.`;
+};
